1733521ac7880aa2068b55fdff53694c15cd6f88
Radovan Dvorsky <dvorsky@visioncraft.cz>
2024-08-01 18:34:51 +0200

    Oprava zobrazenia profilovej fotky

--- src/screens/AddPatient.tsx
+++ src/screens/AddPatient.tsx
@@ -1,15 +1,15 @@
-import React, {useLayoutEffect} from 'react';
-import {StyleSheet} from 'react-native';
-import {Formik} from 'formik';
+import React, { useLayoutEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
-import {getDatabaseConnection} from '../database';
-import {AddPatientProps, Routes} from "../types";
-import {appStyle} from "../theme/AppStyle";
+import { getDatabaseConnection } from '../database';
+import { AddPatientProps, Routes } from "../types";
+import { appStyle } from "../theme/AppStyle";
 import PatientForm from "../forms/PatientForm";
-import {Icon} from 'react-native-elements';
-import {Pressable} from 'native-base';
 import FeButton from "../components/FeButton";
+import fs2 from 'react-native-fs2';
 
+// Definícia rozhrania pre hodnoty pacienta
 export interface PatientValues {
     firstName: string;
     lastName: string;
@@ -23,33 +23,62 @@ export interface PatientValues {
     photo: string;
 }
 
-const AddPatient = ({navigation, route}: AddPatientProps) => {
+// Validačná schéma pre formulár
+const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required('Required'),
+    lastName: Yup.string().required('Required'),
+    diagnosis: Yup.string().optional(),
+    street: Yup.string().required('Required'),
+    city: Yup.string().required('Required'),
+    postalCode: Yup.string().required('Required'),
+    country: Yup.string().required('Required'),
+    birthNumber: Yup.string().required('Required'),
+    doctorId: Yup.number().required('Required')
+});
 
-    const validationSchema = Yup.object().shape({
-        firstName: Yup.string().required('Required'),
-        lastName: Yup.string().required('Required'),
-        diagnosis: Yup.string().optional(),
-        street: Yup.string().required('Required'),
-        city: Yup.string().required('Required'),
-        postalCode: Yup.string().required('Required'),
-        country: Yup.string().required('Required'),
-        birthNumber: Yup.string().required('Required'),
-        doctorId: Yup.number().required('Required')
-    });
+// Funkcia na uloženie obrázka do trvalého úložiska
+const saveImage = async (photoPath: string) => {
+    try {
+        const imageDirectory = `${fs2.DocumentDirectoryPath}/images`;
+        const imageName = `${new Date().getTime()}_photo.jpg`;
+        const newFilePath = `${imageDirectory}/${imageName}`;
 
+        // Vytvorenie priečinka, ak neexistuje
+        if (!await fs2.exists(imageDirectory)) {
+            await fs2.mkdir(imageDirectory);
+        }
+
+        // Kopírovanie súboru
+        await fs2.copyFile(photoPath, newFilePath);
+
+        return newFilePath;
+    } catch (error) {
+        console.error("Error saving image: ", error);
+        return null;
+    }
+};
+
+const AddPatient = ({ navigation, route }: AddPatientProps) => {
     const handleAddPatient = async (values: PatientValues) => {
         const db = await getDatabaseConnection();
-        await db.executeSql(
-            'INSERT INTO Patients (firstName, lastName, diagnosis, address, birthNumber, photo, doctorId) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [values.firstName, values.lastName, values.diagnosis, `${values.street}, ${values.city}, ${values.postalCode}, ${values.country}`, values.birthNumber, values.photo, values.doctorId]
-        );
+        const imagePath = await saveImage(values.photo);
+
+        if (imagePath || values.photo === '') {
+            await db.executeSql(
+                'INSERT INTO Patients (firstName, lastName, diagnosis, address, birthNumber, photo, doctorId) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                [values.firstName, values.lastName, values.diagnosis, `${values.street}, ${values.city}, ${values.postalCode}, ${values.country}`, values.birthNumber, imagePath, values.doctorId]
+            );
+        } else {
+            console.error("Failed to save image, patient not added.");
+        }
+
         navigation.goBack();
     };
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => (
-                <FeButton severity={"tertiary"} title={'Save'} onPress={() => formikRef.current?.submitForm()}/>
+                <FeButton severity={"tertiary"} title={'Save'} onPress={() => formikRef.current?.submitForm()} />
             ),
         });
     }, [navigation]);
@@ -75,7 +104,7 @@ const AddPatient = ({navigation, route}: AddPatientProps) => {
             onSubmit={handleAddPatient}
         >
             {formikProps => (
-                <PatientForm {...formikProps} navigation={navigation}/>
+                <PatientForm {...formikProps} navigation={navigation} />
             )}
         </Formik>
     );
